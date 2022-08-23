c0c8e9d900162fa137682ab6fb5605aab93befee
Маис <Kozannostra777@mail.ru>
2022-08-23 17:41:33 +0300

    moved the creation of Players to a method. Then I invoked the method call through the object of the ChessMain class

--- ToysInChess13-08-2022/ChessMain.java
+++ ToysInChess13-08-2022/ChessMain.java
@@ -1,26 +1,32 @@
+import java.util.ArrayList;
+
 public class ChessMain {
 
     public static void main(String[] args) {
+        // Чистка КОНСОЛИ- полезная штука
+        System.out.print("\033[H\033[J");
+
+        // через создание объекта main класса ChessMain производится вызов метода
+        // createPlayers()
+        // куда мы перенесли создание Игроков
+        ChessMain main = new ChessMain();
+        ArrayList<Player> players = main.createPlayers();
+
+        for (Player player : players) {
+            System.out.println(player);
+        }
+
+    }
+
+    // Создание самих Игроков Вынесено в отдельный метод
+    public ArrayList<Player> createPlayers() {
         Player whitePlayer = new Player("Beth Harmon", "Beth-Harmon@mail.ru", true, 15, 25);
-      
         Player blackPlayer = new Player("Vasilyi Borgov", "VasilyiBorgov@mail.ru", false, 16, 23);
-        
 
-        try{
-        whitePlayer.setRank(2100);
-        blackPlayer.setRank(222120);
-        }
-        catch(IllegalArgumentException err){
-            System.out.println("Пожалуйста введите валидные данные для одного из игроков!!!");
-        }
-                                                            
-
-        System.out.println(whitePlayer);
-        System.out.println(blackPlayer);
-        
-
-
-
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(whitePlayer);
+        players.add(blackPlayer);
+        return players;
 
     }
 
